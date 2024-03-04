3cc326e45656fe4ab4415a78a789ca855602f780
Saleh <saleh.binessa@gmail.com>
2024-03-04 08:26:21 +0300

    Initial Commit

---
+++ WeatherForecastModelwithCodableTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherForecastModelwithCodableTask
+//
+//  Created by Saleh Bin Essa on 04/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
